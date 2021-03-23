937e227f1a757d036b2da3b2fa805046728641fd
alexander <Mkedonsky>
2021-03-23 21:13:21 +0300

    первая домашка

---
+++ src/HomeWork1/HomeWorkApp.java
@@ -0,0 +1,12 @@
+package HomeWork1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+
+    }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
