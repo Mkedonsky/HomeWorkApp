f8d9b9351bfcc650f2676a810feb2ca6c5a89929
alexander <Mkedonsky>
2021-03-25 19:50:12 +0300

    Домашку  исправил

--- src/HomeWork1/HomeWorkApp.java
+++ src/HomeWork1/HomeWorkApp.java
@@ -21,7 +21,7 @@ public class HomeWorkApp {
         int b = - 6;
         int x = a + b;
 
-        if (x >0||x== 0){
+        if (x >=0){
             System.out.println("Сумма положительная");
         }else
        System.out.println("Сумма отрицательная");
@@ -31,7 +31,7 @@ public class HomeWorkApp {
    public static void printColor(){
 
         int value = 101;
-        if (value <0||value==0){
+        if (value <=0){
             System.out.println( "Красный");
         }
         if (value>0 && value<100|| value==100){
@@ -45,7 +45,7 @@ public class HomeWorkApp {
    public static void compareNumbers(){
         int a = 20;
         int b = 21;
-        if (a> b || a == b){
+        if (a>= b){
             System.out.println("a>=b");
         }else
        System.out.println("a<b");
